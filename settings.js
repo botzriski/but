6dec8681875cb7c16a417b23c25adc4b26163576
botzriski <103762207+botzriski@users.noreply.github.com>
2022-05-07 23:30:15 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,9 +5,9 @@ const chalk = require('chalk')
 global.self = false //jadiin true klo gk mau fitur bot lu di pke sama org lain
 
 // setting
-global.ownername ="Riych"
-global.ownernumber = "6281575886399"
-global.botname = "Elaina-Bot"
+global.ownername ="Risky"
+global.ownernumber = "6289531724196"
+global.botname = "Risky-Bot"
 global.thumbnail = fs.readFileSync("./image/Riych.jpg") //sesuaikan dengan nama foto
 global.background = "https://telegra.ph/file/d4c05638fa7886a1d8060.jpg"
 global.lolkey = 'RIFQIBOTZ' //apikey
